f823a1d837838574c94c24e63b30d095682f9f88
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-06-02 18:11:45 +0200

    Add project files

---
+++ src/SplineViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineViewerApp.swift
+//  SplineViewer
+//
+//  Created by Marcel Kwiatkowski on 2024-06-02.
+//
+
+import SwiftUI
+
+@main
+struct SplineViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
